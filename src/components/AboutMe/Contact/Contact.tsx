967ecf7bef76585bd59c67e5b5e3bd7a792744b9
yununyu <myamoly10@gmail.com>
2024-01-17 17:31:58 +0900

    간단한 자기소개 추가. 소개글 업데이트 필요

--- src/components/AboutMe/Contact/Contact.tsx
+++ src/components/AboutMe/Contact/Contact.tsx
@@ -1,41 +1,52 @@
-import {FC, PropsWithChildren} from 'react'
-import {SubTitle, Subheading} from '../../../styles/index'
+import React from 'react'
+import {SubTitle, Subheading, Description, commonStyles} from '../../../styles/index'
+import {PersonalStatement} from '../../index'
 
-type ContactProps = {}
-
-export const Contact: FC<PropsWithChildren<ContactProps>> = props => {
+export const Contact = () => {
   return (
-    <div className="grid grid-rows-1 gap-5 lg:px-6">
-      <SubTitle text="CONTACT"></SubTitle>
-      <div className="leading-7">
-        <p className="break-all">
-          <Subheading text="Email"></Subheading>
-          <span className="text-xs underline">essfree10@gmail.com</span>
-        </p>
-        <p className="break-all">
-          <Subheading text="PhoneNumber"></Subheading>
-          <span className="text-xs underline">010-2305-3385</span>
-        </p>
-        <p className="break-all">
-          <Subheading text="GitHub"></Subheading>
-          <a
-            href="https://github.com/yununyu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-xs underline">
-            https://github.com/yununyu
-          </a>
-        </p>
-        <p className="break-all">
-          <Subheading text="Notion"></Subheading>
-          <a
-            href="https://www.notion.so/0f4499262a7a43f78b0360f09ecbf7b3"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-xs underline">
-            https://www.notion.so/0f4499262a7a43f78b0360f09ecbf7b3
-          </a>
-        </p>
+    <div>
+      <div className="flex flex-col items-start lg:flex-row lg:items-center lg:px-6">
+        <SubTitle text="CONTACT"></SubTitle>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="mt-4 ml-20">
+          {PersonalStatement.map((statement, index) => (
+            <div key={index}>
+              <Description text={statement}></Description>
+            </div>
+          ))}
+        </div>
+
+        <div className="leading-7">
+          <p className="break-all">
+            <Subheading text="Email"></Subheading>
+            <span className="text-xs underline">essfree10@gmail.com</span>
+          </p>
+          <p className="break-all">
+            <Subheading text="PhoneNumber"></Subheading>
+            <span className="text-xs underline">010-2305-3385</span>
+          </p>
+          <p className="break-all">
+            <Subheading text="GitHub"></Subheading>
+            <a
+              href="https://github.com/yununyu"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xs underline">
+              https://github.com/yununyu
+            </a>
+          </p>
+          <p className="break-all">
+            <Subheading text="Notion"></Subheading>
+            <a
+              href="https://www.notion.so/0f4499262a7a43f78b0360f09ecbf7b3"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xs underline">
+              https://www.notion.so/0f4499262a7a43f78b0360f09ecbf7b3
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   )
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@
 export * from './AboutMe/Contact/Contact'
 export * from './AboutMe/SkillSet/SkillSet'
 export * from './AboutMe/Education/Education'
+export * from './Projects/TeamProject/DoT'
+export * from './AboutMe/PersonalStatement/PersonalStatement'
