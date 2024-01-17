28272437e0d7f23bd1ff02e30a06c5647765b575
yununyu <myamoly10@gmail.com>
2024-01-17 12:31:20 +0900

    styles 적용

--- src/components/AboutMe/Contact/Contact.tsx
+++ src/components/AboutMe/Contact/Contact.tsx
@@ -1,5 +1,5 @@
-import {FC, PropsWithChildren, HTMLAttributes} from 'react'
-import {SubTitle} from '../../index'
+import {FC, PropsWithChildren} from 'react'
+import {SubTitle, Subheading} from '../../../styles/index'
 
 type ContactProps = {}
 
@@ -9,17 +9,7 @@ export const Contact: FC<PropsWithChildren<ContactProps>> = props => {
       <SubTitle text="CONTACT"></SubTitle>
       <div className="leading-7">
         <p className="break-all">
-          <span className="mr-2 font-bold text-[#8EB682] text-sm">Facebook</span>
-          <a
-            href="https://github.com/yununyu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-xs underline">
-            https://github.com/yununyu
-          </a>
-        </p>
-        <p className="break-all">
-          <span className="mr-2 font-bold text-[#8EB682] text-sm">Email</span>
+          <Subheading text="Email"></Subheading>
           <a
             href="essfree10@gmail.com"
             target="_blank"
@@ -29,8 +19,28 @@ export const Contact: FC<PropsWithChildren<ContactProps>> = props => {
           </a>
         </p>
         <p className="break-all">
-          <span className="mr-2 font-bold text-[#8EB682] text-sm">PhoneNumber</span>
+          <Subheading text="PhoneNumber"></Subheading>
           <span className="text-xs underline">010-2305-3385</span>
+        </p>
+        <p className="break-all">
+          <span className="mr-2 font-bold text-[#8EB682] text-sm">GitHub</span>
+          <a
+            href="https://github.com/yununyu"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs underline">
+            https://github.com/yununyu
+          </a>
+        </p>
+        <p className="break-all">
+          <Subheading text="Notion"></Subheading>
+          <a
+            href="https://www.notion.so/0f4499262a7a43f78b0360f09ecbf7b3"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs underline">
+            https://www.notion.so/0f4499262a7a43f78b0360f09ecbf7b3
+          </a>
         </p>
       </div>
     </div>
