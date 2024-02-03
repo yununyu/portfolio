b6fbda7d35aa5d0c9b1a220d68e9e22492b53b56
yununyu <myamoly10@gmail.com>
2024-02-03 18:56:26 +0900

    Dot git 주소 변경

--- src/components/Common/Card.tsx
+++ src/components/Common/Card.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, { FC } from 'react'
 import {
   Card,
   CardImg,
@@ -8,26 +8,35 @@ import {
   CardSubtitle,
   Button
 } from 'reactstrap'
-import {Description, Subheading} from '../../styles/index'
+import { Description, Subheading } from '../../styles/index'
 import DoTLogo from '../../assets/DoTLogo.png'
 import github from '../../assets/github.png'
 
 type CardProps = {
   content: string
 }
-export const Cards: FC<CardProps> = ({content}) => {
+export const Cards: FC<CardProps> = ({ content }) => {
   function toGit() {
-    window.location.href = 'https://github.com/your-username/your-repo' //todo - gitRepo 만들어서 여기 업데이트 필요
+    window.location.href = 'https://github.com/yununyu/DoT' //todo - gitRepo 만들어서 여기 업데이트 필요
+  }
+
+  function toSite() {
+    window.location.href = 'http://dot-client.s3-website.ap-northeast-2.amazonaws.com '
   }
   return (
     <div>
-      <Card className="w-full h-full md:w-1/2 lg:w-2/3 xl:w-3/4">
+      <Card className="w-full h-full md:w-1/2 lg:w-2/3 xl:w-3/4 mb-10">
         <CardImg src={DoTLogo} alt="Card image" />
         <CardBody className="d-flex flex-column align-items-center">
           <Description text={content}></Description>
-          <Button onClick={toGit}>
-            <img src={github} alt="To GitHub" className="h-6 w-7" />
-          </Button>
+          <div className='flex-row'>
+            <Button onClick={toGit} className='mr-10'>
+              <img src={github} alt="To GitHub" className="h-6 w-7" />
+            </Button>
+            <Button onClick={toSite}>
+              <img src={DoTLogo} alt="To Site" className="h-6 w-7" />
+            </Button>
+          </div>
         </CardBody>
       </Card>
     </div>
